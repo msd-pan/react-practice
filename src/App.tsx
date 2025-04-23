94aa128535b797cb444860a375bbad438fe03b3d
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-23 15:46:39 +0900

    Onwards to the next project & advanced concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import "./App.css";
-import { Header } from "@/components/header/Header";
-import { CoreConcepts } from "@/components/CoreConcepts";
-import { Examples } from "@/components/Examples";
-
 export const App = () => {
-  return (
-    <div>
-      <Header />
-      <main>
-        <CoreConcepts />
-        <Examples />
-      </main>
-    </div>
-  );
+  return <h1>React Tic-Tac-Toe</h1>;
 };
