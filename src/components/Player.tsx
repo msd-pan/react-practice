3c5b4a647224d7db15a4c07ae0c112e532157faa
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-24 14:04:19 +0900

    Working with state

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+
 type PlayerProps = {
   name: string;
   symbol: string;
 };
 
 export const Player = ({ name, symbol }: PlayerProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" placeholder={name} required />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 };
