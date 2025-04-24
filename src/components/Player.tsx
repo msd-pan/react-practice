e4defa05e12b7c901f70fec3221c7facc02185ac
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-04-24 14:34:40 +0900

    User input & two-way-binding

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -6,16 +6,28 @@ type PlayerProps = {
 };
 
 export const Player = ({ name, symbol }: PlayerProps) => {
+  const [originName, setOriginName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing); // schedules a state update to true
   };
 
-  let playerName = <span className="player-name">{name}</span>;
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOriginName(event.target.value);
+  };
+
+  let playerName = <span className="player-name">{originName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" value={name} required />;
+    playerName = (
+      <input
+        type="text"
+        value={originName}
+        onChange={handleNameChange}
+        required
+      />
+    );
   }
   return (
     <li>
