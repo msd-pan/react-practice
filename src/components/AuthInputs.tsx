5fab8c2a7f6268b06e618ef2c28d418d34dddf4b
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-05-01 15:24:08 +0900

    Scoping css rules with css modules

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -23,7 +23,7 @@ export const AuthInputs = () => {
   return (
     <div id="auth-inputs">
       <div className="controls">
-        <p>
+        <p className="paragraph">
           <label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
           </label>
