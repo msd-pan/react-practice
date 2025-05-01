4dd6aa9c85de14c1699d9a2238656f3959411413
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-05-01 14:04:04 +0900

    Styling react apps with inline styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ export const Header = () => {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{ color: "red", textAlign: "left" }}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 };
