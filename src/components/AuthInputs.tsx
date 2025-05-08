7329d70d8de573b00576073d1780bf3d868a7cbd
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-05-08 15:10:36 +0900

    Styled-components: pseudo selectors, nested rules & media queries

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -28,6 +28,21 @@ const Input = styled.input<{ $invalid: boolean }>`
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 export const AuthInputs = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -77,9 +92,9 @@ export const AuthInputs = () => {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
